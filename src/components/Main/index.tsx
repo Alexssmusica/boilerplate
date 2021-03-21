f80a0822d8e799402324e5140c68858a5681a9d8
Alex Santos de Souza <alexssmusica@gmail.com>
2021-03-21 20:56:10 -0300

    Create Main Component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
+import * as Style from './styles';
+
 const Main = () => (
-    <main>
-        <h1>React Avançado</h1>
-    </main>
+    <Style.Wrapper>
+        <Style.Logo src="/img/logo.svg" alt="Logo" />
+        <Style.Title>React Avançado</Style.Title>
+        <Style.Description>TypeScript, ReactJS, NextJS e Styled Components</Style.Description>
+        <Style.Illustration src="/img/hero-illustration.svg" alt="Imagem ilustartiva" />
+    </Style.Wrapper>
 );
 
 export default Main;
